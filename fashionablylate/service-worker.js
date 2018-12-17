b29e3a5f8e5f40c60c876966c12a54e0ebe3c92d
Steve Francis <steve.byu@gmail.com>
2018-12-17 16:26:16 -0700

    added changes to fashionablylate pwa

--- fashionablylate/service-worker.js
+++ fashionablylate/service-worker.js
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var dataCacheName = 'weatherData-v2';
-var cacheName = 'weatherPWA-final-2';
+var dataCacheName = 'weatherData-v3';
+var cacheName = 'weatherPWA-final-3';
 var filesToCache = [
   '/',
   'index.html',
