1c090a027843d7fbb14424e8aaeb056e4eef1b35
Steve Francis <steve.byu@gmail.com>
2018-12-18 20:37:04 -0700

    added changes to react version of fashionablylate pwa

---
+++ rfl/precache-manifest.0a48f8808cdf16ab32ff4851a49c2fa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "880e1b19e39b9ce64b3d",
+    "url": "/rfl/static/css/main.8ada5b45.chunk.css"
+  },
+  {
+    "revision": "880e1b19e39b9ce64b3d",
+    "url": "/rfl/static/js/main.880e1b19.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/rfl/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "f47fbe0d4a42949deb97",
+    "url": "/rfl/static/js/runtime~main.f47fbe0d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rfl/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2cb5d996d6e0d9c510eed6a052d56181",
+    "url": "/rfl/index.html"
+  }
+];
