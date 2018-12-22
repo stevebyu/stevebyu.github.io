a521595148ccb13c229cd52c797b07803f867a12
Steve Francis <steve.byu@gmail.com>
2018-12-22 08:22:05 -0700

    added changes to react version of saveyermoney pwa

---
+++ rsym/precache-manifest.382feb831956577845ab860e185d71f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "accde46ec06ef6bec0ea",
+    "url": "/rsym/static/css/main.bdf45c77.chunk.css"
+  },
+  {
+    "revision": "accde46ec06ef6bec0ea",
+    "url": "/rsym/static/js/main.accde46e.chunk.js"
+  },
+  {
+    "revision": "61b63230e5b0e76c90bd",
+    "url": "/rsym/static/js/1.61b63230.chunk.js"
+  },
+  {
+    "revision": "311fe8849a0baa05b39d",
+    "url": "/rsym/static/js/runtime~main.311fe884.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rsym/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6cee65816d9ef23419a6944ba933ab2c",
+    "url": "/rsym/index.html"
+  }
+];
--- rsym/service-worker.js
+++ rsym/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f3d48810f636e2fe851872d39c02f1aa.js"
+  "/rsym/precache-manifest.382feb831956577845ab860e185d71f9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/rsym/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
