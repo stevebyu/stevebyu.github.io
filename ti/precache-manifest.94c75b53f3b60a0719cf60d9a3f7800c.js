1e7faa80ea04b24f3616365788dc8a95c20d5c1d
Steve Francis <steve.byu@gmail.com>
2019-01-01 08:12:52 -0700

    added changes to react version of testidb

---
+++ ti/precache-manifest.94c75b53f3b60a0719cf60d9a3f7800c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "28a1cd9353428877ee2b",
+    "url": "/static/css/main.f6704633.chunk.css"
+  },
+  {
+    "revision": "28a1cd9353428877ee2b",
+    "url": "/static/js/main.28a1cd93.chunk.js"
+  },
+  {
+    "revision": "81d64ae8160580c497e5",
+    "url": "/static/js/1.81d64ae8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8122f51fc11d2005e0cf437a0ee62f20",
+    "url": "/index.html"
+  }
+];
