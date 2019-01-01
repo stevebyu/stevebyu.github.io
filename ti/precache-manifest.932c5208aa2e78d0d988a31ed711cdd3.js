4df4b37d038d9e8f35a1b04658e12be5468c2dc2
Steve Francis <steve.byu@gmail.com>
2019-01-01 08:18:28 -0700

    added changes to react version of testidb

---
+++ ti/precache-manifest.932c5208aa2e78d0d988a31ed711cdd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6029f90e771ae1e1e2a",
+    "url": "/ti/static/css/main.f6704633.chunk.css"
+  },
+  {
+    "revision": "a6029f90e771ae1e1e2a",
+    "url": "/ti/static/js/main.a6029f90.chunk.js"
+  },
+  {
+    "revision": "81d64ae8160580c497e5",
+    "url": "/ti/static/js/1.81d64ae8.chunk.js"
+  },
+  {
+    "revision": "9381e6b52fb25bb23d76",
+    "url": "/ti/static/js/runtime~main.9381e6b5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ti/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c5ef00e61626909e541a835c603108b6",
+    "url": "/ti/index.html"
+  }
+];
--- ti/service-worker.js
+++ ti/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0cdc0d9d22a7f0a62de230b867faae51.js"
+  "/ti/precache-manifest.932c5208aa2e78d0d988a31ed711cdd3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ti/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
