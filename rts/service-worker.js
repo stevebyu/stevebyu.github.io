7c0ec979a466de1cbfaddf04be268b917fa511fe
Steve Francis <steve.byu@gmail.com>
2019-01-14 19:59:46 -0700

    added changes to react version of redtruck sightings

--- rts/service-worker.js
+++ rts/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ti/precache-manifest.7fd0c33de6c56a3273d1721ba311081d.js"
+  "/myapp/precache-manifest.e15bb3ad3dfc506b6e0aa59ee05000cd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/ti/index.html", {
+workbox.routing.registerNavigationRoute("/myapp/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
