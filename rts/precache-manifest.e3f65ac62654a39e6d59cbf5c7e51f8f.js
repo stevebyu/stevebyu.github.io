f42e5798a66cb3b3f80adbeaec06e3f40dd694e5
Steve Francis <steve.byu@gmail.com>
2019-02-27 15:33:50 -0700

    added changes to react version of redtruck sightings

---
+++ rts/precache-manifest.e3f65ac62654a39e6d59cbf5c7e51f8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d8612f3d98a94ef25aa",
+    "url": "/rts/static/js/runtime~main.8d8612f3.js"
+  },
+  {
+    "revision": "657ec3ef85242ccffb11",
+    "url": "/rts/static/js/main.657ec3ef.chunk.js"
+  },
+  {
+    "revision": "c8e90d15da207ea51f3b",
+    "url": "/rts/static/js/1.c8e90d15.chunk.js"
+  },
+  {
+    "revision": "657ec3ef85242ccffb11",
+    "url": "/rts/static/css/main.73f183bc.chunk.css"
+  },
+  {
+    "revision": "a72ca229816413b1c97899f6ba14ddba",
+    "url": "/rts/index.html"
+  }
+];
