2123c82f2a1486a2dbb39b3a63414eea2ac85643
Steve Francis <steve.byu@gmail.com>
2019-08-26 14:29:36 -0600

    added changes to react version of toggle my time

---
+++ tmt/precache-manifest.929d68c66abbb6480f63127016fdb38d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91c1d63e9167f5e5c4e07aa85fef609c",
+    "url": "/tmt/index.html"
+  },
+  {
+    "revision": "7ab6b785bdcdcd81e2cb",
+    "url": "/tmt/static/css/main.9db24911.chunk.css"
+  },
+  {
+    "revision": "bfa6e041332b85836845",
+    "url": "/tmt/static/js/2.b6846fd0.chunk.js"
+  },
+  {
+    "revision": "7ab6b785bdcdcd81e2cb",
+    "url": "/tmt/static/js/main.98792cd9.chunk.js"
+  },
+  {
+    "revision": "f26955f333e6bc889346",
+    "url": "/tmt/static/js/runtime~main.53a44255.js"
+  }
+]);
--- tmt/service-worker.js
+++ tmt/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d7a363e796057f007cc848ede9b17fb5.js"
+  "/tmt/precache-manifest.929d68c66abbb6480f63127016fdb38d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tmt/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
