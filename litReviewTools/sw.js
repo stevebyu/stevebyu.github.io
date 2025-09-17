d294ec070f17b3da6ef462f76ee0e515035259d9
Steve Francis <steve.byu@gmail.com>
2025-09-16 18:29:29 -0600

    added manifest and sw

---
+++ litReviewTools/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "pwa-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/app.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Install SW and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Serve cached files when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
